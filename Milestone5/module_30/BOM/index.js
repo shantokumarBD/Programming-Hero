2e4858ef669e225103c645fa50f27411d532a9dd
shanto <shantokumar22@gmail.com>
2026-09-06 13:46:06 +0600

    module 30 bom

---
+++ Milestone5/module_30/BOM/index.js
@@ -0,0 +1,13 @@
+
+
+console.log(location);
+console.log(history);
+
+
+const reload = document.getElementById('btn')
+
+reload.addEventListener('click', ()=>{
+    // console.log(location);
+
+    location.reload()
+})
