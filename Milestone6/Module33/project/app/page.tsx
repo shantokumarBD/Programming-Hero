431bef876ee140cbc24ae1429e976f3520af91fe
shanto <shantokumar22@gmail.com>
2026-09-18 01:28:17 +0600

    next js project setup

---
+++ Milestone6/Module33/project/app/page.tsx
@@ -0,0 +1,11 @@
+import ShowCase from "./showcase/page";
+
+
+export default function Home() {
+  return (
+    <div className="">
+        <h1 className="text-white">hello word</h1>
+        <ShowCase></ShowCase>
+    </div>
+  );
+}
